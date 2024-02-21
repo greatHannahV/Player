137ecffe3421a78be00903dcae461124f03a4efc
greatHannahV <i.kasl@mail.ru>
2024-02-21 16:22:48 +0200

    added playlists

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,5 +1,50 @@
+import { useState, useEffect } from 'react'
+import { apiClient } from '../spotify'
+import '../styles/library.css'
+import { IconContext } from 'react-icons'
+import { AiFillPlayCircle } from 'react-icons/ai'
+
 function Library() {
-  return <div className="screen-container">library</div>
+  const [playlists, setPlaylists] = useState(null)
+  useEffect(() => {
+    apiClient.get('me/playlists').then(function (response) {
+      setPlaylists(response.data.items)
+      console.log(response.data.items)
+    })
+  }, [])
+  const playPlaylist = (id) => {
+    console.log('click')
+  }
+  return (
+    <div className="screen-container">
+      <div className="library-body">
+        {playlists?.map((playlist) => (
+          <div
+            className="playlist-card"
+            key={playlist.id}
+            onClick={() => playPlaylist(playlist.id)}
+          >
+            <img
+              src={
+                playlist?.images[0]?.url ||
+                'https://thumb.ac-illust.com/27/272c6585421e944cd57065da24b6b7b9_t.jpeg'
+              }
+              className="playlist-image"
+              alt="Playlist-Art "
+            />
+            <p className="playlist-title ">{playlist.name}</p>
+
+            <p className="playlist-subtitle ">{playlist.tracks.total} Songs</p>
+            <div className="playlist-fade">
+              <IconContext.Provider value={{ size: '50px', color: '#2e5a4a' }}>
+                <AiFillPlayCircle />
+              </IconContext.Provider>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Library
